04544cf439edbbf70ef8974a11558f8b071a18d1
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-02-03 18:40:02 +0800

    Autostart Emacs daemon in Awesome WM

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sleep 0.1; nitrogen --restore &                         # Restore Nitrogen wallpaper
 picom --experimental-backend &                          # Start Picom
+emacs --daemon                                          # Start the Emacs daemon
 xautolock -time 120 -locker 'systemctl suspend' &       # Suspend the system when the user is inactive for 2 hours
