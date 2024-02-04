0f1b678a826d78353acdb986134785894d38a946
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-02-04 15:50:10 +0800

    Set useless gaps to 15 in Awesome WM

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -57,6 +57,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/mytheme/theme.lua")
+beautiful.useless_gap = 15
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"
