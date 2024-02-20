f0b3dbacca368f95e34eff3a823b0cadf1223519
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-02-20 19:38:05 +0800

    Enable autosuspend

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -3,4 +3,4 @@ sleep 0.1; nitrogen --restore &                         # Restore Nitrogen wallp
 picom --experimental-backend &                          # Start Picom
 emacs --daemon                                          # Start the Emacs daemon
 flameshot                                               # Start Flameshot
-# xautolock -time 120 -locker 'systemctl suspend' &       # Suspend the system when the user is inactive for 2 hours
+xautolock -time 120 -locker 'systemctl suspend' &       # Suspend the system when the user is inactive for 2 hours
