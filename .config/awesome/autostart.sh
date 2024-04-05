92b6104e9a7b2d7a8a041736de3d42ce71eb6134
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-04-05 20:06:15 +0800

    Put user into login screen when auto suspending

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sleep 0.1; nitrogen --restore &                         # Restore Nitrogen wallpaper
-picom --experimental-backend &                          # Start Picom
-emacs --daemon                                          # Start the Emacs daemon
-flameshot                                               # Start Flameshot
-xautolock -time 120 -locker 'systemctl suspend' &       # Suspend the system when the user is inactive for 2 hours
+sleep 0.1; nitrogen --restore &                                # Restore Nitrogen wallpaper
+picom --experimental-backend &                                 # Start Picom
+emacs --daemon                                                 # Start the Emacs daemon
+flameshot                                                      # Start Flameshot
+xautolock -time 120 -locker 'systemctl suspend; slock' &       # Suspend the system when the user is inactive for 2 hours
