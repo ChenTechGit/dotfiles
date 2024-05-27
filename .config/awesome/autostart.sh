cd61c89880c4c6565aa7c42a09375fe90da4572b
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-05-27 17:57:50 +0800

    Autostart Gnome Polkit in Awesome WM

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -3,4 +3,5 @@ sleep 0.1; nitrogen --restore &                                # Restore Nitroge
 picom --experimental-backend &                                 # Start Picom
 emacs --daemon                                                 # Start the Emacs daemon
 flameshot                                                      # Start Flameshot
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 # Start Gnome Polkit
 xautolock -time 120 -locker 'systemctl suspend; slock' &       # Suspend the system when the user is inactive for 2 hours
