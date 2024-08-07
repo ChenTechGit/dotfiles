f9fd8e73adde56c9d48cfc7efc44e5b55db4d8f2
ChenTechGit <Unlit0128@proton.me>
2024-08-07 18:22:57 +0800

    Update Neovim Lualine file

--- .config/nvim/lua/chen/plugins/lualine.lua
+++ .config/nvim/lua/chen/plugins/lualine.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = { "BufReadPre", "BufNewFile" }, -- Only load Lualine when editing a file (dashboard hides lualine)
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- To configure lazy pending updates count
