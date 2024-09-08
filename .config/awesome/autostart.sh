b6446d5d83976db97c2c1da21d66ba739d2efa19
ChenTechGit <Unlit0128@proton.me>
2024-09-08 17:04:48 +0800

    Use Feh instead of Nitrogen as wallpaper setting program in Awesome WM

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sleep 0.1; nitrogen --restore &                                # Restore Nitrogen wallpaper
-picom --experimental-backend &                                 # Start Picom
-emacs --daemon                                                 # Start the Emacs daemon
-flameshot                                                      # Start Flameshot
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 # Start Gnome Polkit
-xautolock -time 120 -locker 'systemctl suspend; slock' &       # Suspend the system when the user is inactive for 2 hours
+sleep 0.1; feh --bg-tile ~/wallpapers/0057.jpg                      # Draw wallpaper
+picom --experimental-backend &                                      # Start Picom
+emacs --daemon                                                      # Start the Emacs daemon
+flameshot                                                           # Start Flameshot
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1      # Start Gnome Polkit
+xautolock -time 120 -locker 'systemctl suspend; slock' &            # Suspend the system when the user is inactive for 2 hours
