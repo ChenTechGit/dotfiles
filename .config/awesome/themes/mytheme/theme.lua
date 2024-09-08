517726c26570e08143d20b123b0b2ffa872d4157
ChenTechGit <Unlit0128@proton.me>
2024-09-08 17:55:43 +0800

    Use JetBrains Nerd Font Mono with a font size of 9.4 in Awesome WM

--- .config/awesome/themes/mytheme/theme.lua
+++ .config/awesome/themes/mytheme/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "MesloLGS Nerd Font Mono 9.4"
+theme.font          = "JetBrains Nerd Font Mono 9.4"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
