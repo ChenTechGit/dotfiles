85dceab10b60703a5935b4ca08406480c314e16b
ChenTechGit <Unlit0128@proton.me>
2024-09-08 17:06:19 +0800

    Added & at the end of flameshot and gnome polkit in startup.sh in Awesome WM

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 0.1; feh --bg-tile ~/wallpapers/0057.jpg                      # Draw wallpaper
 picom --experimental-backend &                                      # Start Picom
-flameshot                                                           # Start Flameshot
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1      # Start Gnome Polkit
+flameshot &                                                         # Start Flameshot
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &    # Start Gnome Polkit
 xautolock -time 120 -locker 'systemctl suspend; slock' &            # Suspend the system when the user is inactive for 2 hours
