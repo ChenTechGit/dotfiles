d5f11dd615ec22ee9ea85120002345fda4e164b4
ChenTechGit <Unlit0128@proton.me>
2024-09-12 18:28:30 +0800

    Turn off relative line numbers

--- .config/nvim/lua/chen/core/options.lua
+++ .config/nvim/lua/chen/core/options.lua
@@ -1,7 +1,6 @@
 local opt = vim.opt
 
 --- Line numbers ---
-opt.relativenumber = true -- Show relative line numbers
 opt.number = true -- Shows absolute line numbers on cursor line (When relative number is off all the other lines show absolute line numbers instead of relative)
 
 --- Tabs and indentation ---
