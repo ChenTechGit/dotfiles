e2de3b0622cae1a26bc18f4759ef2af6ffeeb7dd
ChenTechGit <Unlit0128@proton.me>
2024-09-29 17:44:02 +0800

    Replace t in keymap definitions as well in toggleterm.lua

--- .config/nvim/lua/chen/plugins/toggleterm.lua
+++ .config/nvim/lua/chen/plugins/toggleterm.lua
@@ -28,8 +28,8 @@ return {
     -- Keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc="Split terminal horizontaly" }) -- Split terminal horizontaly
-    keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", { desc="Create a floatting terminal" }) -- Create a floatting terminal
-    vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _btop_toggle()<CR>", {noremap = true, silent = true}) -- Create a floating terminal with Btop in it
+    keymap.set("n", "<leader>ch", "<cmd>ToggleTerm direction=horizontal<CR>", { desc="Split terminal horizontaly" }) -- Split terminal horizontaly
+    keymap.set("n", "<leader>cf", "<cmd>ToggleTerm direction=float<CR>", { desc="Create a floatting terminal" }) -- Create a floatting terminal
+    vim.api.nvim_set_keymap("n", "<leader>ct", "<cmd>lua _btop_toggle()<CR>", {noremap = true, silent = true}) -- Create a floating terminal with Btop in it
   end
 }
