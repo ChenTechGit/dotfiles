43e535da7ef67af9e7cc59c6954e7bec8dccd8c6
ChenTechGit <Unlit0128@proton.me>
2024-10-26 21:10:53 +0800

    Choose witch config to use in Fastfetch

--- .config/bash/conf/start.bash
+++ .config/bash/conf/start.bash
@@ -2,4 +2,14 @@
 ###  START UP COMMANDS ###
 ###########################
 
-fastfetch
+# Section: Fastfetch
+{
+  # Chose which Fastfetch config to use.
+  config="default"
+  # config="compact"
+  if [[ "$config" == "default" ]]; then
+    fastfetch
+  elif [[ "$config" == "compact" ]]; then
+    fastfetch -c ~/.config/fastfetch/compact.jsonc
+  fi
+}
