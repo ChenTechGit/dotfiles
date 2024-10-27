6d612be5158fb3ba1644377a7a8603219bfa3b76
ChenTechGit <Unlit0128@proton.me>
2024-10-27 16:48:41 +0800

    Disable tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,5 +12,8 @@ config.enable_wayland = false -- Wezterm won't show a window if set to true
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 11
 
+-- Disable tab bar
+config.enable_tab_bar = false
+
 -- Return config
 return config
