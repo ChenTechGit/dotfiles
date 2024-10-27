4fd492d459264f9880801fa6d2f697c0d1e33377
ChenTechGit <Unlit0128@proton.me>
2024-10-27 17:12:07 +0800

    Enable transparency

--- .config/nvim/lua/chen/plugins/colorscheme.lua
+++ .config/nvim/lua/chen/plugins/colorscheme.lua
@@ -2,7 +2,16 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000, -- Make sure to load this before all the other start plugins
   config = function()
+    require("tokyonight").setup({
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = true and "transparent" or "dark",
+        floats = true and "transparent" or "dark"
+      }
+    })
+
     -- Load the colorscheme
-    vim.cmd( [[colorscheme tokyonight-night]] )
+    vim.cmd( [[colorscheme tokyonight]] )
   end,
 }
