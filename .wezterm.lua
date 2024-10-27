1bae9d84b8428c9efb6a9a942b9038298c5f1f8f
ChenTechGit <Unlit0128@proton.me>
2024-10-27 16:46:27 +0800

    Setup font

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,12 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- Configuration
+-- Wayland
 config.enable_wayland = false -- Wezterm won't show a window if set to true
+
+-- Font
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 11
 
 -- Return config
 return config
