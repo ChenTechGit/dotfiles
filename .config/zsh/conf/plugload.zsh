704d7e1afc30fefbf8da6238e05dabfa6cc858f0
ChenTechGit <Unlit0128@proton.me>
2024-10-29 13:47:42 +0800

    Don't define ZSH environment variable

--- .config/zsh/conf/plugload.zsh
+++ .config/zsh/conf/plugload.zsh
@@ -2,6 +2,5 @@
 ###  PLUGINS ###
 #################
 
-ZSH=~/.config/zsh
-source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh             # Load Zsh-autosuggestions plugin
-source $ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh     # Load Syntax Highlighting plugin
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh             # Load Zsh-autosuggestions plugin
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh     # Load Syntax Highlighting plugin
