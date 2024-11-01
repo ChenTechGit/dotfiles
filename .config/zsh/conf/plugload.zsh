b5e6e792679b16418ab11b09ef4674d45f0f77ea
ChenTechGit <Unlit0128@proton.me>
2024-11-01 17:43:09 +0800

    Use autopair plugin

--- .config/zsh/conf/plugload.zsh
+++ .config/zsh/conf/plugload.zsh
@@ -4,3 +4,4 @@
 
 source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh             # Load Zsh-autosuggestions plugin
 source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh     # Load Syntax Highlighting plugin
+source ~/.config/zsh/plugins/zsh-autopair/autopair.zsh                               # Load Autopairs plugin
