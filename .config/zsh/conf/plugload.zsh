351f09588d38b6b67250b9340b12ea0aa0b6cd71
ChenTechGit <Unlit0128@proton.me>
2024-11-01 17:45:52 +0800

    Use plugname.plugin.zsh when sourcing plugins

--- .config/zsh/conf/plugload.zsh
+++ .config/zsh/conf/plugload.zsh
@@ -2,6 +2,6 @@
 ###  PLUGINS ###
 #################
 
-source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh             # Load Zsh-autosuggestions plugin
-source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh     # Load Syntax Highlighting plugin
-source ~/.config/zsh/plugins/zsh-autopair/autopair.zsh                               # Load Autopairs plugin
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh             # Load Zsh-autosuggestions plugin
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh     # Load Syntax Highlighting plugin
+source ~/.config/zsh/plugins/zsh-autopair/autopair.plugin.zsh                               # Load Autopairs plugin
