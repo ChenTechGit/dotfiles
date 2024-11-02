70ddd2f90b8f90546f30c1348cbb1512c75f2d83
ChenTechGit <Unlit0128@proton.me>
2024-11-02 21:47:52 +0800

    Only load Lualine when editing a file

--- .config/nvim/lua/config/plugins/lualine.lua
+++ .config/nvim/lua/config/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function()
     local lazy_status = require("lazy.status") -- 󰆙 To configure Lazy pending updates count
