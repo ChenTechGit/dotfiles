73511048f624ee4da3106ac85284c17346a6b927
ChenTechGit <Unlit0128@proton.me>
2024-11-02 16:03:39 +0800

    Add space in tabedit keymap

--- .config/nvim/lua/config/core/keymaps.lua
+++ .config/nvim/lua/config/core/keymaps.lua
@@ -18,7 +18,7 @@ keymap.set("n", "<leader>", "<cmd>close<CR>", { desc = "󰅙 Close current split
 
 -- 󰓩 Tab managment --
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = " Open new tab" })                      --  Open new tab
-keymap.set("n", "<leader>te", ":tabedit", { desc = " Edit file in new tab" })                     --  Edit file in new tab
+keymap.set("n", "<leader>te", ":tabedit ", { desc = " Edit file in new tab" })                    --  Edit file in new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "󰅙 Close current tab" })               -- 󰅙 Close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = " Go to next tab" })                      --  Go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = " Go to previous tab" })                  --  Go to previous tab
