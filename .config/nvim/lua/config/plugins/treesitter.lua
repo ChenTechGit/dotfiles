ac1340c336003df94bb0141c942c8858a6af491f
ChenTechGit <Unlit0128@proton.me>
2024-11-03 11:21:56 +0800

    Setup Treesitter

---
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate", -- 󰁨 Auto update language parsers when plugin is installed
+  dependencies = {
+    "windwp/nvim-ts-autotag"
+  },
+  opts = {
+      highlight = {
+        enable = true
+      },
+      --  Enable indentation
+      indent = { enable = true },
+      --  Enable autotagging (w/ Nvim-ts-autotag plugin)
+      autotag = { enable = true },
+      --   Ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "jsonc",
+        "css",
+        "bash",
+        "lua",
+        "python",
+        "gitignore",
+        "git_rebase",
+        "markdown",
+        "rust",
+        "desktop"
+      },
+      -- Incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>"
+        }
+      }
+    }
+}
