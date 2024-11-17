19364ec4f15203863bdd8647bd099e6cef96be1c
ChenTechGit <Unlit0128@proton.me>
2024-11-17 18:25:07 +0800

    Remove unused configs

--- .config/awesome/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-picom --experimental-backend &                                      # Start Picom
-flameshot &                                                         # Start Flameshot
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &    # Start Gnome Polkit
-xautolock -time 120 -locker 'systemctl suspend; slock' &            # Suspend the system when the user is inactive for 2 hours
