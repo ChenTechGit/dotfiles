f538219ab20b0dcba0f36c5693e7d21a5c9f1557
ChenTechGit <Unlit0128@proton.me>
2024-11-17 17:36:50 +0800

    Change file type to sh when editing files in ~/.local/bin

--- .config/nvim/lua/config/core/ftype.lua
+++ .config/nvim/lua/config/core/ftype.lua
@@ -1,7 +1,11 @@
---  Set filetype to  TOML when editing any .conf files in  Hyprland configuration dir
---  For syntax highlighting
+--  Change file types when openning specific files
 
 vim.cmd [[
+  "  Hyprland
   autocmd BufNewFile,BufRead ~/dotfiles/.config/hypr/*.conf setlocal filetype=toml
   autocmd BufNewFile,BufRead ~/.config/hypr/*.conf setlocal filetype=toml
+
+  "  ~/.local/bin
+  autocmd BufNewFile,BufRead ~/dotfiles/.local/bin/* setlocal filetype=sh
+  autocmd BufNewFile,BufRead ~/.local/bin/* setlocal filetype=sh
 ]]
