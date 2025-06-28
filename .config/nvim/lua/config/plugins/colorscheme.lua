64381fff186096bdbdb05c1848bd477b29f1d5bf
ChenTechGit <Unlit0128@proton.me>
2025-06-28 22:11:28 +0800

    Switched from Tokyonight Catppuccin

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,20 +1,39 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  opts = function()
-    local transparent = true --  Enable transparency :D
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
 
     --  Setup
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      }
-    })
+    opts = function ()
+        require("catppuccin").setup({
+            flavour = "macchiato",
+            transparent_background = true
+        })
 
-    --  Enable colorscheme
-    vim.cmd("colorscheme tokyonight")
-  end
+        --  Setup
+
+        vim.cmd("colorscheme catppuccin")
+    end
 }
+
+-- Old Tokyonight theme
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   opts = function()
+--     local transparent = true --  Enable transparency :D
+--
+--     --  Setup
+--     require("tokyonight").setup({
+--       style = "night",
+--       transparent = transparent,
+--       styles = {
+--         sidebars = transparent and "transparent" or "dark",
+--         floats = transparent and "transparent" or "dark",
+--       }
+--     })
+--
+--     --  Enable colorscheme
+--     vim.cmd("colorscheme tokyonight")
+--   end
+-- }
