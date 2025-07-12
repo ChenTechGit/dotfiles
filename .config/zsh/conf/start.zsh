466fededf42d3bcaadc4b9280466b48a10bf02b3
ChenTechGit <Unlit0128@proton.me>
2025-07-12 21:50:41 +0800

    Added omp config

--- .config/zsh/conf/start.zsh
+++ .config/zsh/conf/start.zsh
@@ -3,6 +3,6 @@
 ###########################
 
 fastfetch
-eval "$(oh-my-posh init zsh)"
+eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/zen.toml)"
 eval "$(zoxide init --cmd cd zsh)"
 eval "$(fzf --zsh)"
