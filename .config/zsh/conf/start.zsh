00c78b6de55b07aab373baba547c4e6388045525
ChenTechGit <Unlit0128@proton.me>
2025-12-07 11:06:33 +0800

    Switch back to Starship

--- .config/zsh/conf/start.zsh
+++ .config/zsh/conf/start.zsh
@@ -3,6 +3,7 @@
 ###########################
 
 # fastfetch
-eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/atomic.json)"
+# eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/atomic.json)"
+eval "$(starship init zsh)"
 eval "$(zoxide init --cmd cd zsh)"
 eval "$(fzf --zsh)"
