dc67428b3f5b25a6776a760cd90345d59a4d62f2
ChenTechGit <Unlit0128@proton.me>
2026-06-01 18:19:53 +0800

    Start Fastfetch upon Zsh startup

--- .config/zsh/conf/start.zsh
+++ .config/zsh/conf/start.zsh
@@ -2,8 +2,8 @@
 ###  START UP COMMANDS ###
 ###########################
 
-# fastfetch
 # eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/atomic.json)"
 eval "$(starship init zsh)"
 eval "$(zoxide init --cmd cd zsh)"
 eval "$(fzf --zsh)"
+fastfetch
